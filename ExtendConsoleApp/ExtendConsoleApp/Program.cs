db3ddd005bf13d9733ee28981b4b869ba7247cf6
loern <fyhzyt@gmail.com>
2023-10-07 19:39:56 +0800

    继承相关知识点

---
+++ ExtendConsoleApp/ExtendConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ExtendConsoleApp
+{
+    class PrivateClass
+    {
+        private int Age { get; set; }
+
+        public void Console()
+        {
+            this.Age = 1;
+        }
+    }
+
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            var privateClass = new PrivateClass();
+            privateClass.
+        }
+    }
+}
