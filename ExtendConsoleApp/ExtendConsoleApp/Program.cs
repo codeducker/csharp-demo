4e7d5de14c62f89d905142a25f77eca5c353ba2f
loern <fyhzyt@gmail.com>
2023-10-09 17:12:51 +0800

    继承相关知识点(修饰符/类型转换判断等)

--- ExtendConsoleApp/ExtendConsoleApp/Program.cs
+++ ExtendConsoleApp/ExtendConsoleApp/Program.cs
@@ -16,17 +16,20 @@ namespace ExtendConsoleApp
     {
         public virtual int Size { get; } = 20;
 
-        private required int Top { get; set; }
+        //private required int Top { get; set; }
 
-        public BasePrivateClass(int size, int top)
-        {
-            Size = size;
-            Top = top;
-        }
+        //public BasePrivateClass(int size, int top)
+        //{
+        //    Size = size;
+        //    Top = top;
+        //}
 
         public void CallBase()
         {
             Console.WriteLine("CallBase");
+        }
+        public void AddComponent()
+        {
         }
     }
 
@@ -37,6 +40,16 @@ namespace ExtendConsoleApp
         {
             Console.WriteLine("CallSec");
         }
+
+        public void AddComponent()
+        {
+        }
+    }
+
+    //抽象类
+    abstract class AbstractPrivateClass
+    {
+        public abstract int GetSize();
     }
 
     //类 单继承 但支持多接口  结构体只支持多接口
@@ -57,6 +70,7 @@ namespace ExtendConsoleApp
 
         public void CallSec()
         {
+            base.CallSec();//调用基类方法
             Console.WriteLine("CallSecPrivate");
         }
 
@@ -64,33 +78,127 @@ namespace ExtendConsoleApp
                                    get; } //重载基类虚属性
     }
 
+    //sealed比较该类无法被继承
+
+    sealed class SealedPrivateClass
+    {
+        public void CheckIs(object o)
+        {
+            //判断是否为 基类的派生类 
+            if (o is IMyClassInterface)
+            {
+                var myClassInterface = o as IMyClassInterface;//类型转换关键字
+                myClassInterface?.FinalMethod();
+            }
+        }
+    }
+
+    //class ExtendSealedPrivateClass : SealedPrivateClass//此时会抛出异常
+    //{
+    //}
+
+    interface IMyClassInterface
+    {
+        public void FinalMethod();
+
+        public virtual void SatGet()
+        {
+            //todo something
+        }
+    }
+
+
+    abstract class AbstractMyClass : IMyClassInterface
+    {
+        public abstract void FinalMethod();
+    }
+
+    class MyClass : AbstractMyClass
+    {
+
+        private int size { set; get; }
+
+        public MyClass()
+        {
+            this.size = 0;
+        }
+
+        public MyClass(int outSize)
+        {
+            this.size = outSize;
+        }
+
+        public override void FinalMethod()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
+    /**
+     * 访问修饰符
+     * public 全部可见
+     * protected 派生类型可见
+     * internal 包含程序集可见
+     * private   所属类型可见
+     * protected internal 包含和派生类型可见
+     */
+
+    class DerivedClass : MyClass
+    {
+
+        protected static int Locked { get; set; }
+
+        public override void FinalMethod()  // wrong. Will give compilation error
+        {
+        }
+
+        public DerivedClass() : base()
+        {
+        }
+
+        //继承基类构造函数
+        public DerivedClass(int outSize) : base(outSize:outSize)
+        {
+        }
+
+        protected internal class InnerDerivedClass
+        {
+            static bool IsLocked()
+            {
+                return Locked == 1 ;
+            }
+        }
+    }
+
     internal class Program
     {
         internal static void Main(string[] args)
         {
-            SecPrivateClass privateClass = new PrivateClass();
-            privateClass.CallBase();
-            privateClass.CallSec();//子类重载时调用方法为子类 ，修饰符为new / 不加入修饰符 此时调用父类方法 ，当修饰符为 override 时 此时调用子类方法
-            var privateClass2 = new PrivateClass();
-            privateClass2.CallBase();
-            privateClass2.CallSec();//此时调用为子类 重写方法
+            var derivedClass = new DerivedClass();
+            var innerDerivedClass = new DerivedClass.InnerDerivedClass();
+            //SecPrivateClass privateClass = new PrivateClass();
+            //privateClass.CallBase();
+            //privateClass.CallSec();//子类重载时调用方法为子类 ，修饰符为new / 不加入修饰符 此时调用父类方法 ，当修饰符为 override 时 此时调用子类方法
+            //var privateClass2 = new PrivateClass();
+            //privateClass2.CallBase();
+            //privateClass2.CallSec();//此时调用为子类 重写方法
 
-            A a;         // 定义一个a这个A类的对象.这个A就是a的申明类
-            A b;         // 定义一个b这个A类的对象.这个A就是b的申明类
-            A c;         // 定义一个c这个A类的对象.这个A就是b的申明类
-            A d;         // 定义一个d这个A类的对象.这个A就是b的申明类
-            a = new A(); // 实例化a对象,A是a的实例类
-            b = new B(); // 实例化b对象,B是b的实例类
-            c = new C(); // 实例化b对象,C是b的实例类
-            d = new D(); // 实例化b对象,D是b的实例类
-            a.Func();    // 执行a.Func：1.先检查申明类A 2.检查到是虚拟方法 3.转去检查实例类A，就为本身 4.执行实例类A中的方法 5.输出结果 Func In A
-            b.Func();    // 执行b.Func：1.先检查申明类A 2.检查到是虚拟方法 3.转去检查实例类B，有重载的 4.执行实例类B中的方法 5.输出结果 Func In B
-            c.Func();    // 执行c.Func：1.先检查申明类A 2.检查到是虚拟方法 3.转去检查实例类C，无重载的 4.转去检查类C的父类B，有重载的 5.执行父类B中的Func方法 5.输出结果 Func In B
-            d.Func();    // 执行d.Func：1.先检查申明类A 2.检查到是虚拟方法 3.转去检查实例类D，无重载的（这个地方要注意了，虽然D里有实现Func()，但没有使用override关键字，所以不会被认为是重载） 4.转 
-            //去检查类D的父类A，就为本身 5.执行父类A中的Func方法 5.输出结果 Func In A
-            D d1 = new D();
-            d1.Func(); // 执行D类里的Func()，输出结果 Func In D
-            Console.ReadLine();
+            //A a;         // 定义一个a这个A类的对象.这个A就是a的申明类
+            //A b;         // 定义一个b这个A类的对象.这个A就是b的申明类
+            //A c;         // 定义一个c这个A类的对象.这个A就是b的申明类
+            //A d;         // 定义一个d这个A类的对象.这个A就是b的申明类
+            //a = new A(); // 实例化a对象,A是a的实例类
+            //b = new B(); // 实例化b对象,B是b的实例类
+            //c = new C(); // 实例化b对象,C是b的实例类
+            //d = new D(); // 实例化b对象,D是b的实例类
+            //a.Func();    // 执行a.Func：1.先检查申明类A 2.检查到是虚拟方法 3.转去检查实例类A，就为本身 4.执行实例类A中的方法 5.输出结果 Func In A
+            //b.Func();    // 执行b.Func：1.先检查申明类A 2.检查到是虚拟方法 3.转去检查实例类B，有重载的 4.执行实例类B中的方法 5.输出结果 Func In B
+            //c.Func();    // 执行c.Func：1.先检查申明类A 2.检查到是虚拟方法 3.转去检查实例类C，无重载的 4.转去检查类C的父类B，有重载的 5.执行父类B中的Func方法 5.输出结果 Func In B
+            //d.Func();    // 执行d.Func：1.先检查申明类A 2.检查到是虚拟方法 3.转去检查实例类D，无重载的（这个地方要注意了，虽然D里有实现Func()，但没有使用override关键字，所以不会被认为是重载） 4.转 
+            ////去检查类D的父类A，就为本身 5.执行父类A中的Func方法 5.输出结果 Func In A
+            //D d1 = new D();
+            //d1.Func(); // 执行D类里的Func()，输出结果 Func In D
+            //Console.ReadLine();
         }
     }
     class A
