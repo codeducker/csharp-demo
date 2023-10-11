709312ac22129b9f5a7d4d615e70ff0b2d128b0d
loern <fyhzyt@gmail.com>
2023-10-11 19:48:29 +0800

    新增泛型知识点

---
+++ GenericConsoleApp/GenericConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericConsoleApp
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+
+        }
+    }
+}
