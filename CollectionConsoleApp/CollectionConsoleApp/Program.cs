a47a7bd4b12774a057c0b3f092f4b78fe75dbe87
loern <fyhzyt@gmail.com>
2023-10-18 18:20:13 +0800

    集合知识点

---
+++ CollectionConsoleApp/CollectionConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionConsoleApp
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
